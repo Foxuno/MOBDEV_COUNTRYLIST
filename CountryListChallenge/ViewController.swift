f7df8b00b365d485cb9aa7f6e5f792b72cee13b2
Foxuno <eduardo.ignacio00@icloud.com>
2024-07-03 14:17:47 -0400

    Initial Commit

---
+++ CountryListChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryListChallenge
+//
+//  Created by Foxunno on 03-07-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
